2db731b586149c48d8f20d326707277dab879d57
Willian <arrudawillian@hotmail.com>
2024-06-05 22:37:13 -0300

    Adicionar arquivos de projeto.

---
+++ SQS/SQS.Consumidor/Program.cs
@@ -0,0 +1,22 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+Console.WriteLine("Hello, World!");
+
+var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+var request = new ReceiveMessageRequest
+{
+    QueueUrl = "https://sqs.sa-east-1.amazonaws.com/211125350833/teste"
+};
+
+while (true)
+{
+    var response = await client.ReceiveMessageAsync(request);
+
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine(message.Body);
+        await client.DeleteMessageAsync("https://sqs.sa-east-1.amazonaws.com/211125350833/teste", message.ReceiptHandle);
+    }
+}
---
+++ SQS/SQS.Fornecedor/Program.cs
@@ -0,0 +1,14 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+Console.WriteLine("Hello, World!");
+
+var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+var request = new SendMessageRequest
+{
+    QueueUrl = "https://sqs.sa-east-1.amazonaws.com/211125350833/teste",
+    MessageBody = "Teste 123"
+};
+
+await client.SendMessageAsync(request);
